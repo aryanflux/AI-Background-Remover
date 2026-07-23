d3adfaa5f2669b42af22c54576d3b2d5338459a5
Aryan <singharyan.work25@gmail.com>
2026-07-23 22:53:26 +0530

    Remove tempCodeRunnerFile from remote

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-from src.image_io import load_image, save_image
-from src.preprocessing import resize_image, convert_to_grayscale
-
-img = load_image("images/inputs/dog image.webp")
-resized = resize_image(img, 512, 512)
-new_img=convert_to_grayscale(resized)
-save_image(new_img, "images/outputs/resized.png")
-
-print("Image resized successfully!")
